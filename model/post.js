e8792e5cb76232e6c4f268e5af6eeffdaed9261c
pengfei.zhou <pengfei.zhou@dianping.com>
2015-06-08 12:26:48 +0800

    提交

--- model/post.js
+++ model/post.js
@@ -90,7 +90,40 @@ Post.getAll = function (name, callback) {
         });
     });
 };
-
+//获取一篇文章
+Post.getOne = function (name, day, title, callback) {
+    //打开数据库
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);
+        }
+        //读取 posts 集合
+        db.collection('posts', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            //根据用户名、发表日期及文章名进行查询
+            collection.findOne({
+                "name": name,
+                "time.day": day,
+                "title": title
+            }, function (err, doc) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                //解析 markdown 为 html
+                try {
+                    doc.post = markdown.toHTML(doc.post);
+                } catch (e) {
+                    console.log('Caught exception: ' + e);
+                }
+                callback(null, doc);//返回查询的一篇文章
+            });
+        });
+    });
+};
 process.on('uncaughtException', function (err) {
     console.log('Caught exception: ' + err);
 });

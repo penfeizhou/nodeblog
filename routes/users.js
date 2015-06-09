bbc6bfd2cc8956b45951b876dad46c3142ebab52
pengfei.zhou <pengfei.zhou@dianping.com>
2015-06-09 13:05:26 +0800

    是用bootstrap模板

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.render('boot', {
+        title: '标题',
+        body: '<p> 正文</p>'
+    });
 });
 
 module.exports = router;
